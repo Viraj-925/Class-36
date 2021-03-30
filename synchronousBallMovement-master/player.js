0fe6ff01ae77a06c1ca23308f0def169dc0f074e
Viraj-925 <76730233+Viraj-925@users.noreply.github.com>
2021-03-30 16:56:48 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/player.js
@@ -0,0 +1,20 @@
+class Player{
+    constructor(){}
+    getCount(){
+        var ply=database.ref('playerCount')
+        ply.on("value",function(data){
+            playerCount=data.val()
+        })
+    }
+    updateCount(count){
+        database.ref('/').update({
+            playerCount:count
+        })
+    }
+    update(name){
+        var playerIndex="player"+playerCount
+        database.ref(playerIndex).set({
+            name:name
+        })
+    }
+}
---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,21 @@
+var hypnoticBall, database;
+var position;
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+ 
+  createCanvas(400,400);
+  game=new Game()
+  game.getState()
+  game.start()
+
+}
+
+function draw(){
+ 
+  
+}
+
